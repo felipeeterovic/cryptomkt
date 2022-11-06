f6d5ce35512af7e0b6118ef383beed0daa5a6781
Felipe Eterovic <felipe.eterovic@gmail.com>
2022-11-06 18:04:55 -0300

    Create order

--- spot_trading.py
+++ spot_trading.py
@@ -6,11 +6,13 @@ class spot_trader:
         #self.at = H256(api_key, api_secret)
         self.api_key = api_key
         self.api_secret = api_secret
+        self.session = requests.session()
+        self.session.auth = (self.api_key, self.api_secret)
+
+
 
     def base(self, url):
-        session = requests.session()
-        session.auth = (self.api_key, self.api_secret)
-        response = session.get(url).json()
+        response = self.session.get(url).json()
         if "error" in response:
             print(f"Error: {response['error']['code']}: {response['error']['message']}")
             return None
@@ -25,3 +27,11 @@ class spot_trader:
     def get_order(self, client_order_id = ""):
         client_order_id = client_order_id if client_order_id = "" else f"/{client_order_id}"
         return self.base(f"https://api.exchange.cryptomkt.com/api/3/spot/order{client_order_id}")
+
+    def create_new_order(self, symbol, side, price, quantity):
+        order_data = {"symbol": symbol.upper(), "side": side, "quantity": quantity, "price": price}
+        response = self.session.post('https://api.exchange.cryptomkt.com/api/3/spot/order/', data = orderData).json()
+        if "error" in response:
+            print(f"Error: {response['error']['code']}: {response['error']['message']}")
+            return None
+        return response
